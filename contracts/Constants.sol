19e4a2c9940f0992508282e407b16d243f49b6dc
Artem Abdullin <artemleelooai@gmail.com>
2022-07-07 21:43:52 +0300

    Added contract Constants.sol

---
+++ contracts/Constants.sol
@@ -0,0 +1,12 @@
+//Constants are variables that cannot be modifided.
+//Their value is hard coded and using constants can save gas cost.
+
+//SPDX-license-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+    //coding convention to uppercase constants variables
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
+
