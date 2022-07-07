6ed8ca3ac8a2457cf0507cf6789c9a59705f6644
Artem Abdullin <artemleelooai@gmail.com>
2022-07-07 20:26:11 +0300

    Added contract HelloWorld.sol

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,9 @@
+// pragma specifies the compiler version of Solodity
+
+// SPDX-License-Indentifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+}
