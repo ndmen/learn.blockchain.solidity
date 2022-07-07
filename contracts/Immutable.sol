378fd65d95ed1c6c450dd52cf28f216b20ac0799
Artem Abdullin <artemleelooai@gmail.com>
2022-07-07 22:00:01 +0300

    Added contract Immutable.sol

---
+++ contracts/Immutable.sol
@@ -0,0 +1,16 @@
+//Immutable variables are like constants. Values of immutable variables can be set inside
+//the constructor but cannot be modifided afterwards.
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Immutable {
+    //coding convention to uppercase constant variables
+    address public immutable MY_ADDRESS;
+    uint public immutable MY_UINT;
+
+    constructor(uint _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
