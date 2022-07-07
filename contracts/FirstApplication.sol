4f1f3b449c26292bbda555fa264cebebfdfdea80
Artem Abdullin <artemleelooai@gmail.com>
2022-07-07 20:34:17 +0300

    Added contract FirstApplication.sol

---
+++ contracts/FirstApplication.sol
@@ -0,0 +1,24 @@
+//Here is a simple contract that you can get, increment and decrement the count store in this contract.
+
+//SPDX-license-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint public count;
+
+    //Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    //Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    //Fucntion to decrement count by 1
+    function dec() public {
+        //This function will fail if count = 0
+        count -= 1;
+    }
+}
