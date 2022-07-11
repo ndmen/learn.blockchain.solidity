f9430d4c06c2d5058ee89c9229be206d040f4a29
Artem Abdullin <artemleelooai@gmail.com>
2022-07-11 04:49:51 +0300

    Added contract CallingParentContracts.sol

---
+++ contracts/CallingParentContracts.sol
@@ -0,0 +1,72 @@
+//Parent contracts can be called directly, or by using the keyword super.
+//By using the keyword super, all of the immediate parent contracts will be called.
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/* Inheritance tree
+   A
+ /  \
+B   C
+ \ /
+  D
+*/
+
+contract A {
+    //This is called an event. You can emit events from your function
+    //and they are logged into the transaction log.
+    //In our case, this will be useful for tracing function calls.
+    event Log(string message);
+
+    function foo() public virtual {
+        emit Log("A.foo called");
+    }
+
+    function bar() public virtual {
+        emit Log("A.bar called");
+    }
+}
+
+contract B is A {
+    function foo() public virtual override {
+        emit Log("B.foo is called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("B.bar is called");
+        super.bar();
+    }
+}
+
+contract C is A {
+    function foo() public virtual override {
+        emit Log("C.foo is called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("C.bar is called");
+        super.bar();    
+    }
+}
+
+contract D is B, C {
+    //Try:
+    //  - Call D.foo and check the transaction logs.
+    //  Although D inherist A, B nad C, it only called C and then A.
+    //  - Call D.bar and check the transaction logs.
+    //  D called C, then B, and finaly A.bar();
+    //  Although super was called twice (by B and C) it only called A once.
+
+    function foo() public override(B, C) {
+        super.foo();
+    }
+
+    function bar() public override(B, C) {
+        super.bar();
+    }
+
+}
+
+
